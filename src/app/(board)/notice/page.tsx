f355e253ccdc379c7b8209d14b13dbf889fd5c10
JangHwanPark <wkd5878@gmail.com>
2023-07-28 01:04:56 +0900

    feat - Pc 네비게이션 드롭다운 메뉴 디자인
    lib/post 네이밍 리팩토링

--- src/app/(board)/notice/page.tsx
+++ src/app/(board)/notice/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {getPosts} from "@/app/lib/post";
+import {getPosts} from "@/app/lib/getPostsFromForum";
 import NoticeItem from "@/components/Notice/NoticeItem";
 import NoticeClientNavbar from "@/components/Notice/LNB/NoticeNavbar";
 import styles from './notice.module.scss';
---
+++ src/app/lib/getPostsFromForum.ts
@@ -0,0 +1,23 @@
+import connectDB from "@/app/lib/mongoDb";
+import {Post} from "@/types/db";
+import {Db} from "mongodb";
+
+const getPosts = async (): Promise<Post[]> => {
+    try {
+        const db: Db = (await connectDB).db("forum");
+        const postCollection = db.collection('post');
+        let posts: Post[] = await postCollection.find().toArray() as Post[];
+
+        posts = posts.map((post: Post) => {
+            post._id = post._id.toString();
+            return post;
+        });
+
+        return posts;
+    } catch(error) {
+        console.error("Failed to get posts:", error);
+        throw error;
+    }
+};
+
+export {getPosts};
