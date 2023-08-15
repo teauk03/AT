981651a247ed5ad88734ae3f5d9e0d908755bf02
JangHwanPark <wkd5878@gmail.com>
2023-08-15 11:47:44 +0900

    Refac - Footer 내부 데이터 Json 분리 및 p -> Link로 변경

--- src/components/UI/Footer/Footer.tsx
+++ src/components/UI/Footer/Footer.tsx
@@ -1,9 +1,9 @@
-"use client";
 import React from 'react';
 import styles from './Footer.module.scss';
 import Image from "next/image";
 import NavigationLogo from "../../../../public/img/home-bg-Transparent.png";
 import Link from "next/link";
+import DATA from '@/data/data-footer-items.json';
 
 
 const FooterClientComponent = () => {
@@ -17,28 +17,14 @@ const FooterClientComponent = () => {
                 </section>
 
                 <nav className={styles['footer-navigate']}>
-                    <div className={styles['footer-navigate-item']}>
-                        <h5>고객서비스</h5>
-                        <p>기기대여</p>
-                        <p>매장위치</p>
-                        <p>커뮤니티</p>
-                    </div>
-                    <div className={styles['footer-navigate-item']}>
-                        <h5>고객지원</h5>
-                        <p>고객지원</p>
-                        <p>대여안내</p>
-                        <p>상담 봇</p>
-                    </div>
-                    <div className={styles['footer-navigate-item']}>
-                        <h5>회사소개</h5>
-                        <p>기업정보</p>
-                        <p>공지사항</p>
-                        <p>이벤트</p>
-                    </div>
-                    <div className={styles['footer-navigate-item']}>
-                        <h5>SNS</h5>
-                        <p>Twitter</p>
-                    </div>
+                    {DATA.FOOTER.map((section, index) => (
+                        <div key={index} className={styles['footer-navigate-item']}>
+                            <h5>{section.title}</h5>
+                            {section.link.map((link, linkIndex) => (
+                                <Link key={linkIndex} href={link.href}>{link.link_title}</Link>
+                            ))}
+                        </div>
+                    ))}
                 </nav>
             </footer>
 
@@ -48,7 +34,7 @@ const FooterClientComponent = () => {
                     <p className={styles.text}>
                         대표: 김00&nbsp;
                         사업자등록번호: 123-45-67890&nbsp;
-                        주소: 어택<br/>
+                        주소: 경기도 부천시 부일로459번길 20 센텀시티 2층<br/>
                         TEL: 010-0000-0000&nbsp;
                         E-mail: da12@attack.com
                     </p>
