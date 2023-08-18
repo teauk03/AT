74b93db8befabe674528bbd215bb07dc9d6b34f7
JangHwanPark <wkd5878@gmail.com>
2023-08-18 11:32:09 +0900

    Refactoring - 글로벌 네비게이션 리팩토링
    CL : 97

--- src/components/UI/NavbarGlobal/GlobalNavItems/GlobalNavItems.tsx
+++ src/components/UI/NavbarGlobal/GlobalNavItems/GlobalNavItems.tsx
@@ -1,48 +1,55 @@
 'use client'
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from "../Navbar.module.scss";
 import Link from "next/link";
-import {usePathname} from "next/navigation";
-import {MenuItemProps} from '@/types/Navigation';
-import {useSession} from "next-auth/react";
+import { usePathname } from "next/navigation";
+import { MenuItemProps, MenuItem } from '@/types/Navigation';
+import { useSession } from "next-auth/react";
 
-const GlobalNavItems = ({gblMenuItems}: MenuItemProps) => {
-    const {data: session} = useSession();
+const GlobalNavItems = ({ gblMenuItems }: MenuItemProps) => {
+    const { data: session } = useSession();
     const currentRoute = usePathname();
     const [showSubMenu, setShowSubMenu] = useState(false);
 
     const handleMouseEnter = () => setShowSubMenu(true);
     const handleMouseLeave = () => setShowSubMenu(false);
 
-    return (
-        <ul className={styles['nav-wrap']} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-            {/* Global Navigation */}
-            {gblMenuItems.map((item) => (
-                <li className={styles['nav-link']} key={item.route}>
-                    <Link href={item.route}
-                          className={`${styles['nav-item']} ${currentRoute === item.route ? styles['active-link'] : styles['non-active-link']}`}>
-                        {item.title}
+    const renderSubMenu = (subMenu: MenuItem[]) => (
+        <ul className={styles['sub-menu']}>
+            {subMenu.map((subItem) => (
+                <li className={styles['sub-nav-link']} key={subItem.route}>
+                    <Link href={subItem.title === "마이페이지" && session?.user?._id ? `/user/mypage/${session.user.name}` : subItem.route} className={styles['sub-nav-item']}>
+                        {subItem.title}
                     </Link>
-                    {/* Mega Navigation Menu */}
-                    {showSubMenu && item.subMenu && item.subMenu.length > 0 && (
-                        <div className={styles['mega-menu']}>
-                            <ul className={styles['sub-menu']} key={item.route}>
-                                {item.subMenu.map((subItem) => (
-                                    <li className={styles['sub-nav-link']} key={subItem.route}>
-                                        <Link
-                                            href={subItem.title === "마이페이지" && session?.user?._id
-                                            ? `/user/mypage/${session.user.name}`
-                                            : subItem.route}
-                                              className={styles['sub-nav-item']}>
-                                            {subItem.title}
-                                        </Link>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    )}
                 </li>
             ))}
+        </ul>
+    );
+
+    const renderMainNavItem = (item: MenuItem) => {
+        const isActive = currentRoute === item.route || (item.subMenu && item.subMenu.some(subItem => currentRoute === subItem.route));
+        return (
+            <li className={styles['nav-link']} key={item.route}>
+                <Link href={item.route}
+                      className={`${styles['nav-item']} ${isActive ? styles['active-link'] : styles['non-active-link']}`}>
+                    {item.title}
+                </Link>
+
+                {/* Mega Navigation Menu */}
+                {showSubMenu && item.subMenu && item.subMenu.length > 0 && (
+                    <div className={styles['mega-menu']}>
+                        {/* renderSubMenu : 서브 메뉴 렌더링 함수 호출 */}
+                        {renderSubMenu(item.subMenu)}
+                    </div>
+                )}
+            </li>
+        );
+    };
+
+    return (
+        <ul className={styles['nav-wrap']} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            {/* Global Navigation : 메인 네비게이션 아이템 렌더링 */}
+            {gblMenuItems.map(renderMainNavItem)}
         </ul>
     );
 };
--- src/types/Navigation.d.ts
+++ src/types/Navigation.d.ts
@@ -1,16 +1,16 @@
 /* [Component - GLB] Navigation */
 import React from "react";
 
-export interface MenuItemProps {
-    gblMenuItems: MenuItem[];
-}
-
 /* [Component - GLB] NavItems
  * [Component - SLB] ForumSlbMenu(SLB_FORUM_ITEMS) */
 export interface MenuItem {
     title: string;
     route: string;
     subMenu?: MenuItem[];
+}
+
+export interface MenuItemProps {
+    gblMenuItems: MenuItem[];
 }
 
 /* [Component] UserLoginMenu */
