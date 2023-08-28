b0ebf87da62f9c81d26f826e9701af973e715821
JangHwanPark <wkd5878@gmail.com>
2023-08-28 20:39:20 +0900

    Refac - svg 아이콘 변경 -> react-icons
    CL : 179

--- src/components/Board/Forum/FormBody/ForumItem.tsx
+++ src/components/Board/Forum/FormBody/ForumItem.tsx
@@ -1,10 +1,10 @@
 'use client'
+import React, {useEffect} from "react";
 import styles from '@/components/Board/Forum/ForumItem.module.scss';
 import Link from "next/link";
 import {pathTypeProps, Post} from "@/types/Borad";
-import SvgIconComponent from "@/components/SvgIconComponent";
 import LoadingForum from "@/components/UI/Loading/LoadingForum";
-import React, {useEffect} from "react";
+import {AiOutlineUser} from "react-icons/ai";
 
 type Result = {
     posts: Post[];
@@ -34,10 +34,7 @@ const ForumItem = ({ path, href }: pathTypeProps) => {
                     <li className={styles['forum-list']} key={index}>
                         {/* User Title */}
                         <div className={styles['author-wrap']}>
-                            <SvgIconComponent
-                                width={20} height={20}
-                                svgPath={'M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z'}
-                            />
+                            <AiOutlineUser/>
                             <div className={styles['post-author']}>
                                 {notice.userName}
                             </div>
--- src/components/UI/Nav/GlobalNavbarComponent.tsx
+++ src/components/UI/Nav/GlobalNavbarComponent.tsx
@@ -10,6 +10,8 @@ import SvgIconComponent from "@/components/SvgIconComponent";
 import IsUserStatusModalMenu from "@/components/UI/Nav/IsUserStatusModalMenu/IsUserStatusModalMenu";
 import AppLink from "@/components/UI/Link/AppLink";
 import NavigationLogo from "../../../../public/img/home-bg-Transparent.png";
+import {FaRegCircleUser} from "react-icons/fa6";
+import {BsBell} from "react-icons/bs";
 import GLOBAL_NAV from "@/data/data-global-nav.json";
 import {MenuItem} from '@/types/Navigation';
 
@@ -139,11 +141,8 @@ const GlobalNavbarComponent = () => {
                             {/* 로그인 상태에서 세션 관련 요소 */}
                             {session?.user &&
                                 <div className={styles['user-session-wrap']} onClick={setIsUserModalClicked}>
-                                    <SvgIconComponent width={25} height={25}
-                                                      svgPath={'M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0'}/>
-                                    <SvgIconComponent width={25} height={25}
-                                                      svgPath={'M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z'}/>
-
+                                    <BsBell/>
+                                    <FaRegCircleUser/>
                                     {/* 클릭시 DropdownMenu 노출 */}
                                     {isMenClicked && <IsUserStatusModalMenu session={session}/>}
                                 </div>
